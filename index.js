0188f92de746c9c302b412cab9f9641cb22f5ed5
Tyler <tylersundquist3@gmail.com>
2022-03-13 20:16:09 -0600

    commit before develop branch, added questions to index.js

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    'What would you like your project title to be?',
+    'What is the description of your project?',
+    'How would you like your project to be installed?',
+    'What will your project be used for?',
+    'What are the contribution details for you project?',
+    'Would you like to include test instructions?',
+    'Please pick a license for your project',
+    'Please provide your GitHub username',
+    'Pleaase provide your email address connected to GitHub'
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
